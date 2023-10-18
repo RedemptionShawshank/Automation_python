419a22c2757608123f11f9525b03a8f3fe142727
RedemptionShawshank <111340684+RedemptionShawshank@users.noreply.github.com>
2023-10-18 13:56:37 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,11 @@
 import re
 
-# Specify the input SQL file name
+# Specify the input SQL file name and output column names file
 input_sql_file = 'input.sql'  # Replace with your file name
+output_column_names_file = 'column_names.txt'  # Name of the output file
 
 # Define a function to extract column names from a DDL statement
 def extract_column_names(ddl_statement):
-    # Use regular expressions to find column names
     column_pattern = r'\s+([^,()]+)\s'
     columns = re.findall(column_pattern, ddl_statement)
     return columns
@@ -16,7 +16,6 @@ ddl_statements = []
 # Read the input SQL file and extract DDL statements
 with open(input_sql_file, 'r') as file:
     sql_content = file.read()
-    # Use a regular expression to split DDL statements
     ddl_statements = re.split('CREATE OR REPLACE TABLE', sql_content)
 
 # Remove empty and whitespace-only entries
@@ -27,14 +26,16 @@ table_columns = {}
 
 # Extract column names for each DDL statement
 for statement in ddl_statements:
-    # Extract the table name
     table_name = statement.split('\n', 1)[0].strip()
-    # Extract column names using the function
     columns = extract_column_names(statement)
-    # Store the column names in the dictionary
     table_columns[table_name] = columns
 
-# Print the extracted column names for each table
-for table, columns in table_columns.items():
-    print(f"Columns for {table}: {columns}")
-  
+# Write the extracted column names to the output file
+with open(output_column_names_file, 'w') as output_file:
+    for table, columns in table_columns.items():
+        output_file.write(f"Columns for {table}:\n")
+        for column in columns:
+            output_file.write(f"{column}\n")
+        output_file.write("\n")
+
+print(f"Column names saved to {output_column_names_file}")
