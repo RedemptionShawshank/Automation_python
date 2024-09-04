a53b24d1c435942483feb7c958d4e7bf1f1dee0c
RedemptionShawshank <111340684+RedemptionShawshank@users.noreply.github.com>
2024-09-04 14:31:52 +0530

    Update keystore.java

--- keystore.java
+++ keystore.java
@@ -100,3 +100,67 @@ public static KeyStore createTrustStoreFromTextFile(String filePath) throws Exce
 
         return trustStore;
     }
+
+
+
+
+public static KeyStore loadKeyStore(String keystorePath, String keystorePassword) throws Exception {
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        try (FileInputStream fis = new FileInputStream(keystorePath)) {
+            keyStore.load(fis, keystorePassword.toCharArray());
+        }
+        return keyStore;
+    }
+    
+    public static KeyStore loadGeneratedKeyStore(String filePath, String keystorePassword) throws Exception {
+        return KeyStoreCreator.createKeyStoreFromTextFile(filePath, keystorePassword);
+    }
+
+
+
+public static boolean compareKeyStores(KeyStore ks1, KeyStore ks2) throws Exception {
+    Enumeration<String> aliases1 = ks1.aliases();
+    while (aliases1.hasMoreElements()) {
+        String alias = aliases1.nextElement();
+        if (!ks2.containsAlias(alias)) {
+            System.out.println("Alias not found in second keystore: " + alias);
+            return false;
+        }
+
+        X509Certificate cert1 = (X509Certificate) ks1.getCertificate(alias);
+        X509Certificate cert2 = (X509Certificate) ks2.getCertificate(alias);
+
+        if (cert1 != null && cert2 != null && !cert1.equals(cert2)) {
+            System.out.println("Certificates for alias " + alias + " do not match.");
+            return false;
+        }
+
+        if (ks1.isKeyEntry(alias)) {
+            if (!ks1.getKey(alias, keystorePassword.toCharArray()).equals(ks2.getKey(alias, keystorePassword.toCharArray()))) {
+                System.out.println("Private keys for alias " + alias + " do not match.");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+public static void main(String[] args) {
+    try {
+        KeyStore originalKeyStore = loadKeyStore("original.jks", "original-password");
+        KeyStore generatedKeyStore = loadGeneratedKeyStore("generated-keystore.txt", "generated-password");
+
+        boolean match = compareKeyStores(originalKeyStore, generatedKeyStore);
+        if (match) {
+            System.out.println("The keystores match.");
+        } else {
+            System.out.println("The keystores do not match.");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+
